e1354501b7aa325a08d723ecc9b1f00e7090f7f4
FelipeMG <felipemontana@gmail.com>
2017-04-13 19:08:52 -0500

    ArithmeticOperations singleton
    ArithmeticOperations with final modifier

--- src/ArithmeticOperations.java
+++ src/ArithmeticOperations.java
@@ -1,5 +1,9 @@
 
-public class ArithmeticOperations {
+public final class ArithmeticOperations {
+	
+	 private ArithmeticOperations() {
+	      throw new AssertionError("This class shouldn't be instantiated");
+	 }
 	
 	public static int gcf(int a, int b) {
 	      return b == 0 ? a : gcf(b, a % b);
