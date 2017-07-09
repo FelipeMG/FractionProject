942c2605998b8a430f6276786cb2c8b38b4de1b6
FelipeMG <felipemontana@gmail.com>
2017-07-09 12:50:52 -0500

    added new operations
    Added minus(), times(), divided(), negative() and inverse() methods.
    Refactor rename project

--- src/Fraction.java
+++ src/Fraction.java
@@ -2,16 +2,33 @@ public class Fraction {
    private int numerator;
    private int denominator;
    public Fraction(int numerator, int denominator) {
-      final int GCF = ArithmeticOperations.gcf(numerator, denominator);
-      this.numerator = numerator / GCF;
-      this.denominator = denominator / GCF;
+	   final int GCF = ArithmeticOperations.gcf(numerator, denominator);
+	   this.numerator = numerator / GCF;
+	   this.denominator = denominator / GCF;
    }
    public Fraction sum(Fraction that) {
-	      final int LCM = ArithmeticOperations.lcm(this.denominator, that.denominator);
-	      final int numerator = LCM / this.denominator * this.numerator
-	            + LCM / that.denominator * that.numerator;
-	      return new Fraction(numerator, LCM);
-   }   
+	   final int LCM = ArithmeticOperations.lcm(this.denominator, that.denominator);
+	   final int numerator = LCM / this.denominator * this.numerator 
+		   + LCM / that.denominator * that.numerator;
+	   return new Fraction(numerator, LCM);
+   }
+   public Fraction minus(Fraction that) {
+	   return this.sum(that.negative());
+   }
+   public Fraction times(Fraction that) {
+	   final int numerator = this.numerator * that.numerator;
+	   final int denominator = this.denominator * that.denominator;
+	   return new Fraction(numerator, denominator);
+   }
+   public Fraction divided(Fraction that) {
+	   return this.times(that.inverse());
+   }
+   public Fraction negative() {
+	   return new Fraction(this.numerator * -1, this.denominator);
+   }
+   public Fraction inverse() {
+	   return new Fraction(this.denominator, this.numerator);
+   }
    public int getNumerator() {
       return numerator;
    }
