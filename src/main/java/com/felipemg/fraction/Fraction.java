c15d63c2efdb5140b3338be18ac0595959cd19e2
FelipeMG <felipemontana@gmail.com>
2018-02-20 23:24:11 -0500

    added FractionTest

--- src/main/java/com/felipemg/fraction/Fraction.java
+++ src/main/java/com/felipemg/fraction/Fraction.java
@@ -1,47 +1,83 @@
 package com.felipemg.fraction;
 
-public class Fraction {
-   private int numerator;
-   private int denominator;
-   public Fraction(int numerator, int denominator) {
-	   if(denominator != 0){		
-		   final int GCF = ArithmeticOperations.gcf(numerator, denominator);
-		   this.numerator = numerator / GCF;
-		   this.denominator = denominator / GCF;		   
-	   }
-	   else{
-		   throw new IllegalArgumentException("Denominator can't be 0");
-	   }
-   }
-   public Fraction plus(Fraction that) {
+import com.felipemg.fraction.util.ArithmeticOperations;
+
+public final class Fraction {
+
+    private int numerator;
+    private int denominator;
+
+    private Fraction(int numerator, int denominator) {
+        if(denominator != 0){
+            final int GCF = ArithmeticOperations.gcf(numerator, denominator);
+            this.numerator = numerator / GCF;
+		    this.denominator = denominator / GCF;
+        }
+	    else{
+		    throw new IllegalArgumentException("Denominator can't be 0");
+	    }
+    }
+
+    public static Fraction of(final int numerator, final int denominator){
+        return new Fraction(numerator, denominator);
+    }
+
+    public static Fraction of(final int numerator){
+        return new Fraction(numerator, 1);
+    }
+
+    public Fraction plus(Fraction that) {
 	   final int LCM = ArithmeticOperations.lcm(this.denominator, that.denominator);
 	   final int numerator = LCM / this.denominator * this.numerator 
 		   + LCM / that.denominator * that.numerator;
 	   return new Fraction(numerator, LCM);
-   }
-   public Fraction minus(Fraction that) {
+    }
+
+    public Fraction minus(Fraction that) {
 	   return this.plus(that.negative());
    }
-   public Fraction times(Fraction that) {
+
+    public Fraction times(Fraction that) {
 	   final int numerator = this.numerator * that.numerator;
 	   final int denominator = this.denominator * that.denominator;
 	   return new Fraction(numerator, denominator);
    }
+
    public Fraction divided(Fraction that) {
 	   return this.times(that.inverse());
    }
+
    public Fraction negative() {
 	   return new Fraction(this.numerator * -1, this.denominator);
    }
+
    public Fraction inverse() {
 	   return new Fraction(this.denominator, this.numerator);
    }
-   public int getNumerator() {
-      return numerator;
-   }
-   public int getDenominator() {
-      return denominator;
-   }
-   
-   
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Fraction fraction = (Fraction) o;
+
+        if (numerator != fraction.numerator) return false;
+        return denominator == fraction.denominator;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = numerator;
+        result = 31 * result + denominator;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Fraction{" +
+                "numerator=" + numerator +
+                ", denominator=" + denominator +
+                '}';
+    }
 }
--- src/test/java/com/felipemg/fraction/FractionTest.java
+++ src/test/java/com/felipemg/fraction/FractionTest.java
@@ -1,4 +1,76 @@
 package com.felipemg.fraction;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.Test;
+
 public class FractionTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void twoOverZeroIsNotAFraction(){
+        Fraction twoOverZero = Fraction.of(2, 0);
+        assertThat(twoOverZero, is(twoOverZero));
+    }
+
+    @Test
+    public void twoFourthsIsOneHalf(){
+        Fraction twoFourths = Fraction.of(2, 4);
+        Fraction oneHalf = Fraction.of(1, 2);
+        assertThat(twoFourths, is(oneHalf));
+    }
+
+    @Test
+    public void oneHalfPlus1IsThreeHalves(){
+        Fraction oneHalf = Fraction.of(1, 2);
+        Fraction oneUnit = Fraction.of(1);
+        Fraction threeHalves = Fraction.of(3, 2);
+        assertThat(oneHalf.plus(oneUnit), is(threeHalves));
+    }
+
+    @Test
+    public void threeFourthsMinusOneHalfIsOneFourth(){
+        Fraction threeFourths = Fraction.of(3, 4);
+        Fraction oneFourth = Fraction.of(1, 4);
+        Fraction oneHalf = Fraction.of(1, 2);
+        assertThat(threeFourths.minus(oneHalf), is(oneFourth));
+    }
+
+    @Test
+    public void oneFifthTimes3IsThreeFifths(){
+        Fraction threeFifths = Fraction.of(3, 5);
+        Fraction oneFifth = Fraction.of(1, 5);
+        Fraction three = Fraction.of(3);
+        assertThat(oneFifth.times(three), is(threeFifths));
+    }
+
+    @Test
+    public void twoThirdsDivided2IsOneThird(){
+        Fraction twoThirds = Fraction.of(2, 3);
+        Fraction oneThird = Fraction.of(1, 3);
+        Fraction two = Fraction.of(2);
+        assertThat(twoThirds.divided(two), is(oneThird));
+    }
+
+    @Test
+    public void negativeOfOneHalfIsMinusOneHalf(){
+        Fraction oneHalf = Fraction.of(1, 2);
+        Fraction minusOneHalf = Fraction.of(-1, 2);
+        assertThat(oneHalf.negative(), is(minusOneHalf));
+    }
+
+    @Test
+    public void inverseOfTwoThirdsIsThreeHalves(){
+        Fraction twoThirds = Fraction.of(2, 3);
+        Fraction threeHalves = Fraction.of(3, 2);
+        assertThat(twoThirds.inverse(), is(threeHalves));
+    }
+
+    @Test
+    public void inverseOfMinusTwoThirdsIsMinusThreeHalves(){
+        Fraction minusTwoThirds = Fraction.of(-2, 3);
+        Fraction minusThreeHalves = Fraction.of(-3, 2);
+        assertThat(minusTwoThirds.inverse(), is(minusThreeHalves));
+    }
+
 }
